57e1cd0fb6faba21cafd61a753bf48a99070e0f8
MAJK Home <MAJKHome@outlook.com>
2026-03-12 14:51:51 +0500

    Add project files.

---
+++ Models/AppModels.cs
@@ -0,0 +1,17 @@
+﻿namespace AppliedInvoice.Models
+{
+    public class AppModels
+    {
+    }
+
+    public class InvoiceMaster
+    {
+        public string invoiceType { get; set;  }
+    }
+
+    public class InvoiceDetails
+    {
+        public string hsCode { get; set; }
+        public decimal quantity { get; set; }
+    }
+}
